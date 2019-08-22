708c0ab9f500c26d38f8080ee0768e073e50fbff
Pierre Narvor <pnarvor@laas.fr>
2019-08-22 18:41:03 +0200

    [refactor] Added a setup.py script

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(name='nephelae',
+      version='0.1',
+      description='Core functionnalities of Nephelae project real-time mapping system.',
+      url='ssh://git@redmine.laas.fr/laas/users/simon/nephelae/nephelae-devel/nephelae_base.git',
+      author='Pierre Narvor',
+      author_email='pnarvor@laas.fr',
+      licence='bsd3',
+      packages=find_packages(include=['nephelae*']),
+      install_requires=[
+        'numpy',
+        'scipy',
+        'utm',
+        'matplotlib',
+        'sh'
+      ],
+      zip_safe=False)
+
+
