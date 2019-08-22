0b97cfbb4a1f842ec689d38e52e2053e657be775
Pierre Narvor <pnarvor@laas.fr>
2019-08-22 18:53:38 +0200

    [refactor/array] Copied generic array type from nephelae_simulation/mesonh_interface

---
+++ nephelae/array/__init__.py
@@ -0,0 +1,7 @@
+from .DimensionHelper   import DimensionBounds
+from .DimensionHelper   import DimensionHelper
+from .ScaledArray       import ScaledArray
+from .PeriodicContainer import PeriodicContainer
+
+
+
