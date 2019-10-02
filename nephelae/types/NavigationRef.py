a5fea6fd4cc8aa264b4b7a8b4ddc01a62a9bd865
Pierre Narvor <pnarvor@laas.fr>
2019-10-02 11:33:38 +0200

    [doc][types] Added documentation for NavigationRef type

--- nephelae/types/NavigationRef.py
+++ nephelae/types/NavigationRef.py
@@ -2,17 +2,66 @@ from .Position import Position
 
 class NavigationRef:
 
+    """
+    NavigationRef
+
+    Local frame position. Roughly a copy of Paparazzi NAVIGATION_REF message.
+    TODO make this independent of Paparazzi.
+
+    Attributes
+    ----------
+    position : types.Position
+        GPS position of the local frame in UTM coordinate system.
+        position.t : Origin of time (start of mission ?)
+        position.x : UTM east coordinate.
+        position.y : UTM north coordinate.
+        position.z : Altitude (from sea level).
+
+    utm_zone : str?, int?
+        UTM zone of the local frame (NOT USED).
+
+    Methods
+    -------
+    __getattr__(name) -> float:
+        Retrieve either 'stamp', 'utm_east', 'utm_north' or 'alt' of
+        reference frame. 'stamp' is the mission start time.
+    """
 
     def __init__(self, position=Position(0,0,0,0), utm_zone=31):
+        """
+        Parameters
+        ----------
+        position : types.Position
+            GPS position of the local frame in UTM coordinate system.
+            position.t : Origin of time (start of mission ?)
+            position.x : UTM east coordinate.
+            position.y : UTM north coordinate.
+            position.z : Altitude (from sea level).
+
+        utm_zone : str?, int?
+            UTM zone of the local frame (NOT USED, can be omited without
+            consequences).
+        """
         self.position = position
         self.utm_zone = utm_zone
-
+        
 
     def __str__(self):
         return "NavigationRef : "+str(self.position)+", utm_zone : "+str(self.utm_zone)
 
 
     def __getattr__(self, name):
+        """
+        Retrieve either 'stamp', 'utm_east', 'utm_north' or 'alt' of
+        GPS position. Respectively aliases to self.position.t,
+        self.position.x, self.position.y ans self.position.z.
+
+        Parameters
+        ----------
+
+        name : str
+            Name of the attribute to retrieve.
+        """
         # For compatibility with paparazzi Gps type
         if name == 'stamp':
             return self.position.t
