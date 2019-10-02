57cdfff79c93b8153f4c70e286b4e5e60b687d48
Pierre Narvor <pnarvor@laas.fr>
2019-10-02 11:41:49 +0200

    [doc][types] Added a comment in __init__.py

--- nephelae/types/__init__.py
+++ nephelae/types/__init__.py
@@ -1,10 +1,16 @@
+"""
+nephelae.type package
+
+Contains a bunch of base types used in Nephelae libraries.
+"""
+
 from .Position      import Position
 from .SensorSample  import SensorSample
 from .Gps           import Gps
 from .NavigationRef import NavigationRef
 
-from .DataShape import DimensionShape
-from .DataShape import Shape4D
+from .DataShape import DimensionShape # deprecated
+from .DataShape import Shape4D # deprecated
 from .Bounds    import Bounds
 
 from .ObserverPattern import ObserverSubject
