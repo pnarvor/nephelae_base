3fe6ac734a432fe6c603bc9b59b8a16a82243dfd
Pierre Narvor <pnarvor@laas.fr>
2019-10-15 16:14:46 +0200

    [tests] Updated database_replay tests

--- tests/database/database_replay01.py
+++ tests/database/database_replay01.py
@@ -9,12 +9,22 @@ import time
 from ivy.std_api import *
 import logging
 
-from nephelae_mapping.database import DatabasePlayer
+from nephelae.database import DatabasePlayer
 
-from helpers.helpers import *
+class Logger:
+
+    def __init__(self):
+        pass
+
+    def add_sample(self, sample):
+        print(sample, end="\n\n")
+
+    def add_gps(self, gps):
+        print(gps, end="\n\n")
 
 
-dtbase = DatabasePlayer('output/database01.neph')
+# dtbase = DatabasePlayer('output/database01.neph')
+dtbase = DatabasePlayer('/home/pnarvor/work/nephelae/data/temp/dt5_01_1.neph')
 
 logger = Logger()
 dtbase.add_gps_observer(logger)
