80870b90ce34f40f446f6abd3804c05d0e39ac22
Pierre Narvor <pnarvor@laas.fr>
2019-11-19 16:22:07 +0100

    [scenario] Updated Scenario (set db navFrame, connected uavs to db)

--- nephelae_scenario/Scenario.py
+++ nephelae_scenario/Scenario.py
@@ -4,7 +4,7 @@ import time
 
 from nephelae.types             import NavigationRef, Position, Pluginable
 from nephelae.database          import NephelaeDataServer
-from nephelae_paparazzi         import build_aircraft, AircraftLogger
+from nephelae_paparazzi         import build_aircraft
 from nephelae_paparazzi.plugins import WindSimulation
 
 from .YamlParser import YamlParser
@@ -26,14 +26,13 @@ class Scenario(Pluginable):
         
         self.parser = YamlParser(mainConfigPath)
         
-        self.missionTime = None
+        self.missionT0   = None
         self.localFrame  = None
         self.flightArea  = None
         self.aircrafts   = {}
         self.database    = None
         self.mesonhFiles = None
         self.maps        = None
-        self.logger      = AircraftLogger(quiet=True)
         self.running     = False
 
 
@@ -42,8 +41,8 @@ class Scenario(Pluginable):
         self.config = self.parser.parse()
 
         # Using current time as global mission time
-        self.missionTime = time.time()
-        ref = Position(self.missionTime,
+        self.missionT0 = time.time()
+        ref = Position(self.missionT0,
                        self.config['local_frame']['east'],
                        self.config['local_frame']['north'],
                        self.config['local_frame']['alt'])
@@ -51,6 +50,7 @@ class Scenario(Pluginable):
         self.flightArea = self.config['flight_area']
         
         self.database = NephelaeDataServer()
+        self.database.set_navigation_frame(self.localFrame)
         # TODO
         # try:
         #     if self.config['database']['enable_save']:
@@ -65,9 +65,9 @@ class Scenario(Pluginable):
         for key in self.config['aircrafts']:
             aircraft = build_aircraft(str(key), self.localFrame,
                                       self.config['aircrafts'])
-            # aircraft.add_gps_observer(self.logger)
-            # aircraft.add_status_observer(self.logger)
-            # aircraft.add_sensor_observer(self.logger)
+            aircraft.add_gps_observer(self.database)
+            if hasattr(aircraft, 'add_sensor_observer'):
+                aircraft.add_sensor_observer(self.database)
             self.aircrafts[str(key)] = aircraft
 
         if 'wind_feedback' in self.config.keys():
