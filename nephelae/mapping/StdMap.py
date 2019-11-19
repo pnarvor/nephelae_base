1d9c911217e045a1d4a51566a183f11e493388c2
Florian Seguin <fseguin@laas.fr>
2019-11-19 11:27:46 +0100

    [gpr-rework] Added empty class StdMap, to complete

---
+++ nephelae/mapping/StdMap.py
@@ -0,0 +1,10 @@
+import numpy as np
+import threading
+from sklearn.gaussian_process import GaussianProcessRegressor
+
+from nephelae.types import Bounds
+
+from .MapInterface import MapInterface
+
+class StdMap(MapInterface):
+    pass
