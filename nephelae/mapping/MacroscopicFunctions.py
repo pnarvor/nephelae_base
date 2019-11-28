75c18ded1b38d1e8ce5281716f71fb1de6e9f5ab
Florian Seguin <fseguin@laas.fr>
2019-11-28 14:10:41 +0100

    Added in MacroscopicFunctions, the computation of borders

--- nephelae/mapping/MacroscopicFunctions.py
+++ nephelae/mapping/MacroscopicFunctions.py
@@ -15,7 +15,8 @@ def compute_com(scaledArr):
     Returns
     ---------
     Tuple (1xN)
-        Returns the C.O.M. in the coordinates of the map
+        Returns the C.O.M. in the coordinates of the map.
+        /!\ Returns None if the sum of the scaledArr.data is 0 /!\
     """
     res = None
     data = scaledArr.data.ravel()
@@ -26,3 +27,50 @@ def compute_com(scaledArr):
         center_of_mass = np.sum(data*locations, axis=1)/x
         res = scaledArr.dimHelper.to_unit(center_of_mass)
     return res
+
+def threshold_array(arr, threshold=1e-5):
+    """
+    Thresholds an array, returning a binary array
+
+    Parameters
+    ---------
+    arr : NumpyArray
+        Contains the data to threshold
+    Returns
+    ---------
+    NumpyArray
+        Returns arr with binary values, depending of the threshold
+    """
+    arr[arr < threshold] = 0.0
+    arr[arr >= threshold] = 1.0
+    return arr
+
+def compute_cross_section_border(scaledArr_data, scaledArr_std, factor=1,
+        threshold=1e-5):
+    """
+    Computes the border of the scaledArr, using a threshold, and returns inner
+    and outer borders, using std values with a certain confidence (given by the
+    factor parameter)
+
+    Parameters
+    ---------
+    scaledArr_data : ScaledArray
+        Contains the data of interest
+    scaledArr_std : ScaledArray
+        Contains std associated with the data
+    factor : number
+        Gives the confidence of the std. See Gaussian properties with the std.
+    threshold : number
+        Gives the number where values are nullified (0) or not (1)
+
+    Returns
+    --------
+    NumpyArray, NumpyArray
+        Returns inner and outer borders of the data.
+    """
+    inner_border, outer_border = (scaledArr_data.data - factor *
+            scaledArr_std.data, scaledArr_data.data + factor *
+            scaledArr_std.data)
+    threshold_arr(inner_border, threshold)
+    threshold_arr(outer_border, threshold)
+    return inner_border, outer_border
