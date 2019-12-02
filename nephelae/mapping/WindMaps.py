85050d69c34c39cf151b6d43fafb575cdbbffec7
Pierre Narvor <pnarvor@laas.fr>
2019-12-02 18:21:51 +0100

    [mapping] Added a guard type in WindMwp constant

--- nephelae/mapping/WindMaps.py
+++ nephelae/mapping/WindMaps.py
@@ -19,7 +19,7 @@ class WindMapConstant(MapInterface):
     def __init__(self, variableName, wind=[0.0,0.0],
                  resolution=[50.0,50.0,50.0,50.0]):
         super().__init__(variableName)
-        self.wind  = wind
+        self.wind  = np.array(wind)
         self.resol = resolution
 
 
