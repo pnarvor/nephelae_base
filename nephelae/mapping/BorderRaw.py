91c77657b43c628e91f8627cede07f06d709f922
Florian Seguin <fseguin@laas.fr>
2019-12-06 18:42:03 +0100

    Added class BorderRaw (uses only one map, no incertitude)

---
+++ nephelae/mapping/BorderRaw.py
@@ -0,0 +1,23 @@
+from scipy import ndimage
+import numpy as np
+
+from nephelae.array import ScaledArray
+
+from .FactoryBorder import FactoryBorder
+
+class BorderRaw(FactoryBorder):
+    def __init__(self, name, mapInterface):
+        super().__init__(name)
+        self.mapInterface = mapInterface
+
+    def compute_borders(self, arrays):
+        raw_data = arrays.data
+        border = threshold_array(raw_data).astype(np.int32)
+        eroded = ndimage.binary_erosion(border).astype(border.dtype)
+        border_raw = np.bitwise_xor(border, eroded)
+        raw_scarray = ScaledArray(border_raw, arrays.dimHelper,
+                arrays.interpolation)
+        return raw_scarray
+
+    def get_arrays(self, keys):
+        return self.mapInterface.__getitem__(keys)
