f01b57475d57ba9e09b8a0798cf683de7454534f
Pierre Narvor <pnarvor@laas.fr>
2019-12-12 10:53:16 +0100

    [examples] Updated mission example

--- examples/basic_mission.py
+++ examples/basic_mission.py
@@ -6,7 +6,7 @@ from nephelae_scenario         import Scenario
 from nephelae_paparazzi.common import IvyStop, messageInterface
 from nephelae_paparazzi.utils  import send_lwc
 
-configFilename = '/home/pnarvor/work/nephelae/files-config/mission_only_200.yaml'
+configFilename = 'config/missions_only_200.yaml'
 scenario = Scenario(configFilename)
 scenario.load()
 scenario.start()
@@ -17,7 +17,7 @@ rules    = factory.parameterRules
 print(rules['start'].summary())
 
 def create():
-    aircraft.create_mission('Lace', start=[747.24, 1078.94, 700.0])
+    aircraft.create_mission('Rosette', start=[747.24, 1078.94, 515.0])
 
 
 def stop():
