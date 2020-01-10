9ce46fbdbad6473200f5c973fb4c3375e4064ee9
Pierre Narvor <pnarvor@laas.fr>
2020-01-10 12:15:55 +0100

    [types] Fixed the way NavigationRef is handling utm_zone

--- nephelae_scenario/Scenario.py
+++ nephelae_scenario/Scenario.py
@@ -66,17 +66,16 @@ class Scenario(Pluginable):
                        localFrameConfig['east'],
                        localFrameConfig['north'],
                        localFrameConfig['alt'])
-        self.localFrame = NavigationRef(ref, localFrameConfig['utm_zone'],
-                localFrameConfig['utm_letter'])
+        self.localFrame = NavigationRef(ref, localFrameConfig['utm_zone'])
         self.flightArea = self.config['flight_area']
         
         self.database = self.configure_database()
 
         # To be configured in config file
-        self.windMap = WindMapConstant('Horizontal Wind', [0.0, 0.0])
+        # self.windMap = WindMapConstant('Horizontal Wind', [0.0, 0.0])
         # self.windMap = WindMapConstant('Horizontal Wind', [-7.5, -0.5])
-        # self.windMap = WindObserverMap('Horizontal Wind', sampleName=str(['UT','VT']))
-        # self.database.add_sensor_observer(self.windMap)
+        self.windMap = WindObserverMap('Horizontal Wind', sampleName=str(['UT','VT']))
+        self.database.add_sensor_observer(self.windMap)
             
         if 'mesonh_files' in self.config.keys():
             self.mesonhFiles   = self.config['mesonh_files']
