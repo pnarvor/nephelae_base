292ccdd752686dce00384b0a0f556997887f8824
Pierre Narvor <pnarvor@laas.fr>
2020-01-30 09:56:22 -0400

    [dataviews] Moved dataviews types to another folder

---
+++ nephelae/dataviews/types/__init__.py
@@ -0,0 +1,17 @@
+# Dataviews are type meant to handle the interface between the database and
+# data consumers. Their purpose includes abstracting database access (several
+# ways of accessing data depending on the consumer), do simple data
+# pre-processing (sensor calibration...), while being able to change their
+# behavior online.
+
+from .DataView            import DataView
+from .DatabaseView        import DatabaseView
+from .TimeView            import TimeView
+from .Function            import Function
+from .Scaling             import Scaling
+from .HumidityCalibration import HumidityCalibration
+
+
+
+
+
